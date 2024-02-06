10d50c676691a65b4968ec16ee3a67144e3fbb10
vikashkumar6207 <vktd828403@gmail.com>
2024-02-06 15:40:45 +0530

    Array, DOM

--- javaScriptClasses/DOM/script.js
+++ javaScriptClasses/DOM/script.js
@@ -7,7 +7,19 @@ headerTitle.textContent = 'BignarCode';
 
 // headerTitle.innerHTML = '<h1>Hello</h1>'
 
-headerTitle.style.borderBottom = '5px solid red';
+// headerTitle.style.borderBottom = '5px solid red';
 
-var header = document.getElementById('header');
-header.style.borderBottom = '10px solid green';
+// var header = document.getElementById('header');
+// header.style.borderBottom = '10px solid green';
+
+var item = document.getElementsByClassName('list-item');
+// console.log(item[0]);
+item[0].textContent = 'CSS';
+item[3].textContent = 'javaScript';
+item[1].style.color = 'red';
+item[2].style.fontWeight = 'bold';
+item[3].style.backgroundColor = 'yellow';
+for(let key = 0;key < item.length;key++){
+   item[key].style.backgroundColor = 'yellow';
+
+}
