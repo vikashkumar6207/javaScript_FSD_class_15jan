970e5ab13ee3a38058d4bcb267a59050f9216959
vikashkumar6207 <vktd828403@gmail.com>
2024-02-23 14:06:44 +0530

    some change

--- Projects/Stoper_Watch/script.js
+++ Projects/Stoper_Watch/script.js
@@ -24,6 +24,7 @@ stopBtn.addEventListener('click', function(){
 resetBtn.addEventListener('click', function(){    
     clearInterval(timerId);
     timerDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 00;
     
 });
 
