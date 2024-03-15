501232318301650f3ceca34e4d0822bcf731af18
vikashkumar6207 <vktd828403@gmail.com>
2024-03-15 16:51:31 +0530

    Promise APi

--- javaScriptClasses/18.AsyncJs/6.PromiseApi/script.js
+++ javaScriptClasses/18.AsyncJs/6.PromiseApi/script.js
@@ -1,98 +1,72 @@
-// Question: How can you Replicate API response with PROMISES with Delay of 5 seconds;
+// --------------- PROMISE APIS   -----------------
+
+// Promise has 6 Static methods
 
 /*
-// PROMISE CREATION
-const pokemonServerPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve({
-      name: "pikachu",
-      ability: "lightning thunder",
-    });
-  }, 5000);
-});
+  1. Promise.all
+  
+  I need a list of Promise That need to be resolved simontaneously.
 
-// PROMISE consumption : can only happen IF PROMISE REJECT OR RESOLVE.
-
-pokemonServerPromise.then((data) => {
-  console.log(data, "pokemon data");
-});
-
-setTimeout(() => {
-  console.log("after 5 sec");
-}, 5000);
+  All the Promise nnedds to RESOLVE otherwise ALL THE PROMISE WILL be rejected.
 
 */
 
-//  ---------------- --------------- Promises are single Value ---------------
-
-/*
-// 1. PROMISE CREATION
-const promise = new Promise((resolve, rej) => {
-  resolve("lio");
-  resolve("abc");
-  resolve("def");
+let payerPromise = new Promise((res, rej) => {
+  // Payer had 200 rs with him, he is paying Shopkeeper 100 rs.
+  res(100); // balance lafet with payer
 });
 
-// 2. PROMISE CONSUMPTION
-promise
+let shopKeeperPromise = new Promise((res, rej) => {
+  res(10000 + 100);
+
+  // rej("didnt get money"); // 10000
+});
+
+// payerPromise.then(asdsadas) / shopKeeperPromise.then(asdadasda);
+
+// Insted of doing this
+
+Promise.all([payerPromise, shopKeeperPromise])
   .then((data) => {
-    console.log(data, "Promises are single Value leve 1"); // lio NO def NO ABC
+    // data // [payerPromiseData, shopKeeperPromiseData] // [100, 10100]
+    // RBI (reserve bank of India Father of all the banks)
+    // RBI take a ledger
+    // payer - 100
+    // shopkeeper - 10100
+
+    console.log(data, "RBI will take a ledger");
   })
-  .then((data) => {
-    console.log(data, "Promises are single Value level 2");
+  .catch((error) => {
+    // RBI CAN DECIDE TO PAY BACK THE PAYER
+    console.log(error, "RBI SHOULD ROLLBACK THE MONEY TO PAYER");
   });
 
-  OBSERVATION: we can only resolve one value only from Promise.
+/*
+    2. Promise.race 
+
+    List of all the Promise and will check who won.
 
   */
 
-// ------------------------ Promise are Eager -----------------
+const googlePromise = new Promise((res) => {
+  //const data =  XMLHttpRequest(google.com) 1 second  // data from google
+  // res(data);
 
-// PROMISE CREATION
-
-/*
-
-console.log("Start of the doom");
-
-const fetchPromise = new Promise((resolve) => {
-  console.log("inside the promise");
-
-  // MoCking the API CALL (RESULT delay of 5 seconds )
   setTimeout(() => {
-    resolve("resolved DATA");
+    res("data from google ");
+  }, 1000);
+
+  // google server is respononding with "data" after 1 second
+});
+
+const utkarshWebsitePromise = new Promise((res) => {
+  setTimeout(() => {
+    res("data from utkarsh website");
   }, 5000);
+
+  // utkasrrh server is respononding with "data" after 5 second
 });
 
-console.log("Outside subscribing");
-
-// PROMISE CONSUMPTION
-
-fetchPromise.then((data) => console.log("handling result of : " + data));
-
-console.log("after resolution of Promise");
-
-*/
-
-// ------------ SetTimeout Vs Promise -------------
-// MicrotaskQueue vs MacrotaskQueue
-/*
-  1. Event loop ask question from the Stack (Are you empty)
-  2. Event loop ask question from MICROTASK QUEUE (Promise Queue). DO you have any item ? 
-    2.1 YES -> That item will be Entertained and MACROTASK QUEUE will not be entertained after that.
-    2.2 NO -> THen ONLY Ill event loop will ask a question to MACROTASK QUEUE.
-
-*/
-
-const promise = new Promise((res) => {
-  res(1);
+Promise.race([googlePromise, utkarshWebsitePromise]).then((data) => {
+  console.log("data inside the race", data);
 });
-
-setTimeout(() => {
-  console.log("hello");
-}, 0);
-
-promise.then((data) => {
-  console.log(data);
-});
-
-// Answer: 1 hello;
